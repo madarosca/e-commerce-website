3c427e33d60eb94bdc896382f6bd50a924e5d2fc
Madalina Rosca <madalina@neho.ch>
2022-11-21 16:42:24 +0200

    Section 5
    add react router
    add home page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import Main from './components/main/main.component'
+import Home from './routes/home/home.component'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
-	const categories = [
-		{
-			id: 1,
-			title: 'Hats',
-			imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-		},
-		{
-			id: 2,
-			title: 'Jackets',
-			imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-		},
-		{
-			id: 3,
-			title: 'Sneakers',
-			imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-		},
-		{
-			id: 4,
-			title: 'Womens',
-			imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-		},
-		{
-			id: 5,
-			title: 'Mens',
-			imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-		},
-	]
-
-	return <Main categories={categories} />
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+		</Routes>
+	)
 }
 
 export default App
