877b0b3036ea921868a3c0985eff098c1b7848f3
Madalina Rosca <madalina@neho.ch>
2022-11-21 17:21:45 +0200

    Section 5
    add navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Home from './routes/home/home.component'
 import { Routes, Route } from 'react-router-dom'
+
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<Home />} />
+			<Route path='/' element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path='/shop' element={<div>Shop page</div>} />
+			</Route>
 		</Routes>
 	)
 }
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import './navigation.styles.scss'
+
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+
+import { ReactComponent as ShopLogo } from '../../assets/logo.svg'
+
+const Navigation = () => {
+	return (
+		<Fragment>
+			<div className='navigation'>
+				<Link className='logo-container' to='/'>
+					<ShopLogo className='logo' />
+				</Link>
+				<div className='nav-links-container'>
+					<Link className='nav-link' to='/shop'>
+						SHOP
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</Fragment>
+	)
+}
+
+export default Navigation
