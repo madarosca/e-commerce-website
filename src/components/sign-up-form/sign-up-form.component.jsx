f5dabd02f24d74cd1318fcb1707009d940d2dab7
Madalina Rosca <madalina@neho.ch>
2022-12-02 22:25:06 +0200

    add context to authentication

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,17 @@
 import './navigation.styles.scss';
 
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
-
+import { UserContext } from '../../contexts/user.context';
 import { ReactComponent as ShopLogo } from '../../assets/logo.svg';
 
 const Navigation = () => {
+	const { currentUser, setCurrentUser } = useContext(UserContext);
+
+	const handleSignOut = () => {
+		setCurrentUser(null);
+	};
+
 	return (
 		<Fragment>
 			<div className='navigation'>
@@ -22,12 +28,20 @@ const Navigation = () => {
 					>
 						SHOP
 					</Link>
-					<Link
-						className='auth'
-						to='/auth'
-					>
-						SIGN IN
-					</Link>
+					{!!currentUser ? (
+						<Link
+							className='auth'
+							to='/'
+							onClick={handleSignOut}
+						>
+							SIGN OUT
+						</Link>
+					) : (
+						<Link
+							className='auth'
+							to='/auth'
+						>SIGN IN</Link>
+					)}
 				</div>
 			</div>
 			<Outlet />
