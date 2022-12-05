919830b00b2b47fb03d7ad155d2c796fe79fef03
Madalina Rosca <madalina@neho.ch>
2022-12-05 22:09:09 +0200

    added add to card feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-	const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+	const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
 	const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
@@ -14,7 +14,7 @@ const CartIcon = () => {
 			onClick={toggleIsCartOpen}
 		>
 			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>0</span>
+			<span className='item-count'>{cartCount}</span>
 		</div>
 	);
 };
