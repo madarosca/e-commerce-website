9ef37443d013d852565765b98dbeb225b49f9057
Madalina Rosca <madalina@neho.ch>
2022-12-11 21:57:52 +0200

    change components to categories
    display categories preview on main shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,19 @@
 import { useContext } from 'react';
-import ProductCard from '../../components/product-card/product-card.component';
-import { ProductsContext } from '../../contexts/products.context';
+import { CategoriesContext } from '../../contexts/categories.context';
 import './shop.styles.scss';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 const Shop = () => {
-	const { products } = useContext(ProductsContext);
+	const { categoriesMap } = useContext(CategoriesContext);
 
 	return (
-		<div className='products-container'>
-			{products.map((product) => (
-				<ProductCard
-					key={product.id}
-					product={product}
-				></ProductCard>
+		<div className='shop-container'>
+			{Object.keys(categoriesMap).map((title) => (
+				<CategoryPreview
+					key={title}
+					title={title}
+					products={categoriesMap[title]}
+				/>
 			))}
 		</div>
 	);
