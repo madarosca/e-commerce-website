748c45307e4251739190d62e4cc034394afb98b8
Madalina Rosca <madalina@neho.ch>
2022-12-11 22:20:50 +0200

    remove console.log

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -13,7 +13,6 @@ export const CategoriesProvider = ({ children }) => {
 		const getCategoriesMap = async () => {
 			const categoryMap = await getCategoriesAndDocuments();
 			setCategoriesMap(categoryMap);
-			console.log(categoryMap);
 		};
 
 		getCategoriesMap();
