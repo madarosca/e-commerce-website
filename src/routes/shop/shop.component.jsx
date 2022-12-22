b80bb81d87398765b5e1278891a2d9017dd2aa27
Madalina Rosca <madalina@neho.ch>
2022-12-22 22:38:26 +0200

    refactor code for fetching categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import { setCategoriesMap } from '../../store/categories/categories.action';
+import { setCategories } from '../../store/categories/categories.action';
 
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
@@ -12,8 +12,8 @@ const Shop = () => {
 
 	useEffect(() => {
 		const getCategoriesMap = async () => {
-			const categoryMap = await getCategoriesAndDocuments();
-			dispatch(setCategoriesMap(categoryMap));
+			const categories = await getCategoriesAndDocuments('categories');
+			dispatch(setCategories(categories));
 		};
 
 		getCategoriesMap();
