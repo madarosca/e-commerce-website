d532e40b6296e3c7abe368c59b9ed0b2b5069aca
Madalina Rosca <madalina@neho.ch>
2022-12-22 21:52:30 +0200

    create user reducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import { Routes, Route } from 'react-router-dom';
 
+import { createUserDocumentFromAuth, onAuthStateChangedListener } from './utils/firebase/firebase.utils';
+import { setCurrentUser } from './store/user/user.action';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
@@ -7,6 +12,19 @@ import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const unsuscribe = onAuthStateChangedListener((user) => {
+			if (user) {
+				createUserDocumentFromAuth(user);
+			}
+			dispatch(setCurrentUser(user));
+		});
+
+		return unsuscribe;
+	});
+
 	return (
 		<Routes>
 			<Route
---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+	currentUser: null,
+};
+
+export const USER_ACTION_TYPES = { SET_CURRENT_USER: 'SET_CURRENT_USER' };
+
+export const userReducer = (state = INITIAL_STATE, action) => {
+	const { type, payload } = action;
+
+	switch (type) {
+		case USER_ACTION_TYPES.SET_CURRENT_USER:
+			return {
+				currentUser: payload,
+			};
+		default:
+			return state
+	}
+};
