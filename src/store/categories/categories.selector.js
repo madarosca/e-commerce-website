5eb3220c02f2cc97ca864ceba92fd814cf4f69c6
Madalina Rosca <madalina@neho.ch>
2022-12-27 21:43:08 +0200

    add reselect library

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,3 +1,5 @@
+import { createSelector } from 'reselect';
+
 export const selectCategoriesMap = (state) =>
 	state.categories.categories.reduce((acc, category) => {
 		const { title, items } = category;
