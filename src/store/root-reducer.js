22295af748e42180e882017a5a64ea7c4422139a
Madalina Rosca <madalina@neho.ch>
2022-12-27 21:42:52 +0200

    update redux store

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,13 +1,8 @@
-// import { combineReducers } from 'redux';
 import { categoriesReducer } from './categories/categories.reducer';
 import { userReducer } from './user/user.reducer';
 
-// in the future, use RTK
+// use RTK
 export const rootReducer = {
 	user: userReducer,
 	categories: categoriesReducer,
 };
-
-// export const rootReducer = combineReducers({
-// 	user: userReducer,
-// });
--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,23 @@
-// import { compose, createStore, applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-// in the future, use RTK
+const loggerMiddleware = (store) => (next) => (action) => {
+	if (!action.type) {
+		return next(action);
+	}
+
+	console.log({ type: action.type, payload: action.payload, currentState: store.getState() });
+
+	next(action);
+
+	console.log('next state: ', store.getState());
+};
+
+// use RTK
 export const store = configureStore({
 	reducer: rootReducer,
-	middleware: [logger],
+	middleware: [loggerMiddleware],
 	// middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
-
-// const middleWares = [logger];
-
-// const composedEnhancers = compose(applyMiddleware(...middleWares));
-
-// export const store = createStore(rootReducer, undefined, composedEnhancers);
