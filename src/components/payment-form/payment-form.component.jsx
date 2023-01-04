594bb9cac80fb51d866c9a55fe6a7368256afdf1
Madalina Rosca <madalina@neho.ch>
2023-01-04 17:20:24 +0200

    wip add stripe payments

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -10,6 +10,16 @@ const PaymentForm = () => {
 		e.preventDefault();
 
 		if (!stripe || !elements) return;
+
+		const response = await fetch('/.netlify/functions/create-payment-intent', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ amount: 10000 }),
+		}).then((res) => res.json());
+
+		console.log(response);
 	};
 
 	return (
