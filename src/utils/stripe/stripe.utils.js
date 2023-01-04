16c07320077dedd2339343e61da3217992874807
Madalina Rosca <madalina@neho.ch>
2023-01-04 17:53:01 +0200

    change stripe key for netlify

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,3 +1,5 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+export const stripePromise = loadStripe(
+	'sk_test_51MMWOiFyDPr57FafssW7HWAyEJWVASrzcs1tJgwsYyQu0wdEdpCTu8IcLSncXdlwsmdFhMeRuTcGKWveJcbHoHJh00eKosCSG1'
+);
