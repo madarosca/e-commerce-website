91587432d60640ed447446180346432bb632d83f
Madalina Rosca <madalina@neho.ch>
2023-01-04 10:52:12 +0200

    create sagas for user auth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-
 import { Routes, Route } from 'react-router-dom';
 
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from './utils/firebase/firebase.utils';
-import { setCurrentUser } from './store/user/user.action';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
+import { checkUserSession } from './store/user/user.action';
 
 const App = () => {
 	const dispatch = useDispatch();
-
 	useEffect(() => {
-		const unsuscribe = onAuthStateChangedListener((user) => {
-			if (user) {
-				createUserDocumentFromAuth(user);
-			}
-			dispatch(setCurrentUser(user));
-		});
-
-		return unsuscribe;
+		dispatch(checkUserSession());
 	});
 
 	return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -89,3 +89,16 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+
+export const getCurrentUser = () => {
+	return new Promise((resolve, reject) => {
+		const unsubscribe = onAuthStateChanged(
+			auth,
+			(userAuth) => {
+				unsubscribe();
+				resolve(userAuth);
+			},
+			reject
+		);
+	});
+};
