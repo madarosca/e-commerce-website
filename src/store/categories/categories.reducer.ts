02a1d573896145310d5fd031e61cf0bdbcf8fe7c
Madalina Rosca <madalina@neho.ch>
2023-01-13 14:00:49 +0200

    type cart reducer

--- src/store/categories/categories.reducer.ts
+++ src/store/categories/categories.reducer.ts
@@ -1,6 +1,6 @@
 import { AnyAction } from 'redux';
 import { Category } from './categories.types';
-import { fetchCategoriesStart, fetchCategoriesSuccess, fetchCategoriesFailed  } from './categories.action';
+import { fetchCategoriesStart, fetchCategoriesSuccess, fetchCategoriesFailed } from './categories.action';
 
 export type CategoriesState = {
     readonly categories: Category[];
@@ -9,26 +9,26 @@ export type CategoriesState = {
 }
 
 const CATEGORIES_INITIAL_STATE: CategoriesState = {
-	categories: [],
-	isLoading: false,
-	error: null,
+    categories: [],
+    isLoading: false,
+    error: null,
 }
 
 export const categoriesReducer = (
-  state = CATEGORIES_INITIAL_STATE,
-  action = {} as AnyAction
+    state = CATEGORIES_INITIAL_STATE,
+    action: AnyAction
 ): CategoriesState => {
     if (fetchCategoriesStart.match(action)) {
         return { ...state, isLoading: true }
     }
-    
+
     if (fetchCategoriesSuccess.match(action)) {
         return { ...state, categories: action.payload, isLoading: false }
     }
-    
+
     if (fetchCategoriesFailed.match(action)) {
         return { ...state, error: action.payload, isLoading: false }
     }
-    
+
     return state;
 }
--- src/store/categories/categories.types.ts
+++ src/store/categories/categories.types.ts
@@ -1,12 +1,12 @@
 export enum CATEGORIES_ACTION_TYPES {
-	FETCH_CATEGORIES_START = 'categories/FETCH_CATEGORIES_START',
-	FETCH_CATEGORIES_SUCCESS = 'categories/FETCH_CATEGORIES_SUCCESS',
-	FETCH_CATEGORIES_FAILED = 'categories/FETCH_CATEGORIES_FAILED',
-}
+    FETCH_CATEGORIES_START = 'categories/FETCH_CATEGORIES_START',
+    FETCH_CATEGORIES_SUCCESS = 'categories/FETCH_CATEGORIES_SUCCESS',
+    FETCH_CATEGORIES_FAILED = 'categories/FETCH_CATEGORIES_FAILED',
+};
 
 export type CategoryItem = {
     id: number;
-    imageUrls: string;
+    imageUrl: string;
     name: string;
     price: number;
 }
